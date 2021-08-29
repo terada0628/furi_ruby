b0680c8ee64f11d98c3cb907513e7868a37f1888
terada0628 <ntpeanuts@yahoo.co.jp>
2021-08-29 08:20:32 +0000

    6clear

---
+++ chapter3/6-1.rb
@@ -0,0 +1,5 @@
+(1..9).each do |x|
+  (1..9).each do |y|
+    puts x * y
+  end
+end
---
+++ chapter3/6-2.rb
@@ -0,0 +1,5 @@
+(1..9).each do |x|
+  (1..9).each do |y|
+    puts x.to_s + "Ｘ" + y.to_s + "=" + (x * y).to_s
+  end
+end
