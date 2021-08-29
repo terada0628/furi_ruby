4bf81b26a95ac63ff6e55e1fd9d04516a11dc20b
terada0628 <ntpeanuts@yahoo.co.jp>
2021-08-29 10:00:00 +0000

    4clear

---
+++ chapter4/4-1.rb
@@ -0,0 +1,3 @@
+data = {:name => "山本", :bill => 40000}
+puts data[:name]
+puts data[:bill]
---
+++ chapter4/4-2.rb
@@ -0,0 +1,7 @@
+data = [
+  {name: "山本", bill: 40000},
+  {name: "吉田", bill: 25000}
+  ]
+
+puts data[1][:name]
+puts data[1][:bill]
