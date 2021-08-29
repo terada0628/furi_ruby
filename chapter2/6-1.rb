2101b74890b54646837b47ce07f28d001057c50b
terada0628 <ntpeanuts@yahoo.co.jp>
2021-08-29 04:17:46 +0000

    6clear

---
+++ chapter2/6-1.rb
@@ -0,0 +1,5 @@
+puts "年齢は？"
+age = gets.to_i
+if age >= 6 && age <= 15
+  puts "義務教育の対象"
+end
---
+++ chapter2/6-3.rb
@@ -0,0 +1,10 @@
+text = "abc"
+if ! text.empty?
+  puts "空ではない"
+end
+
+# if → unless に変えても意味は同じ
+# text = "abc"
+# unless text.empty?
+#   puts "空ではない"
+# end
